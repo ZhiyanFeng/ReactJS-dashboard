8a1604abcca54a6d5e8e056df0e183b83f3dcba0
DANIEL CHEN <daniel85.chen@gmail.com>
2016-03-08 04:22:42 -0800

    12th commit

--- config/environment.rb
+++ config/environment.rb
@@ -4,8 +4,6 @@ require File.expand_path('../application', __FILE__)
 require 'yaml'
 require 'json'
 
-ENV['RAILS_ENV']="development"
-
 ENV_VARS = YAML.load_file("#{Rails.root}/config/environment_variables.yml")
 
 # Initialize the Rails application.

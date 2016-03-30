daf5138ce664c7273626ff232b91558660e5c8cd
DANIEL CHEN <daniel85.chen@gmail.com>
2016-03-29 19:46:33 -0700

    tracked push user mession

--- app/models/channel.rb
+++ app/models/channel.rb
@@ -81,22 +81,18 @@ class Channel < ActiveRecord::Base
     end
 
     begin
-      targets = User.joins(:subscription, :mession).where("subscriptions.channel_id = #{post_object[:channel_id]} AND subscriptions.user_id != #{post_object[:owner_id]} AND subscriptions.is_valid AND subscriptions.is_active AND messions.is_active AND subscriptions.subscription_mute_notifications = 'f'")
+      @targets = User.joins(:subscription, :mession).where("subscriptions.channel_id = #{post_object[:channel_id]} AND subscriptions.user_id != #{post_object[:owner_id]} AND subscriptions.is_valid AND subscriptions.is_active AND messions.is_active AND subscriptions.subscription_mute_notifications = 'f'")
       @cpr = ChannelPushReport.create(
         :channel_id => post_object[:channel_id],
-        :target_number => targets.count,
+        :target_number => @targets.count,
         :attempted => 0,
         :success => 0,
         :failed_due_to_missing_id => 0,
         :failed_due_to_other => 0
       )
-      targets.each do |user|
+      @targets.each do |@user|
         TrackedPushNotificationWorker.perform_async(
-          user[:id],
-          user[:first_name],
-          user[:last_name],
-          user.mession[:push_to],
-          user.mession[:push_id],
+          @user
           post_object[:id],
           post_object[:content],
           post_object[:channel_id],
--- app/workers/tracked_push_notification_worker.rb
+++ app/workers/tracked_push_notification_worker.rb
@@ -2,36 +2,36 @@ class TrackedPushNotificationWorker
   include Sidekiq::Worker
   sidekiq_options queue: "default"
   # sidekiq_options retry: false
-  def perform(user_id,user_first_name,user_last_name,user_push_to,user_push_id,post_id,post_content,post_channel_id,cpr_id,post_archtype,base_type)
+  def perform(user,post_id,post_content,post_channel_id,cpr_id,post_archtype,base_type)
     response = 0
     if post_archtype
-      message = "#{user_first_name} #{user_last_name} posted a shift trade request. Interested in helping out?"
-      response = tracked_subscriber_push("open_app", message, 4, post_id, user_id, post_channel_id, user_push_to, user_push_id)
+      message = "#{user[:first_name]} #{user[:last_name]} posted a shift trade request. Interested in helping out?"
+      response = user.mession.tracked_subscriber_push("open_app", message, 4, post_id, user[:id], post_channel_id, user.mession[:push_to], user.mession[:push_id])
     elsif base_type == "announcement"
-      message = user_first_name + " " + user_last_name + " announced: " + post_content
-      response = tracked_subscriber_push("open_detail", message, 4, post_id, user_id, post_channel_id, user_push_to, user_push_id)
+      message = user[:first_name] + " " + user[:last_name] + " announced: " + post_content
+      response = user.mession.tracked_subscriber_push("open_detail", message, 4, post_id, user[:id], post_channel_id, user.mession[:push_to], user.mession[:push_id])
     elsif base_type == "post"
-      message = user_first_name + " " + user_last_name + " posted: " + post_content
-      response = tracked_subscriber_push("open_detail", message, 4, post_id, user_id, post_channel_id, user_push_to, user_push_id)
+      message = user[:first_name] + " " + user[:last_name] + " posted: " + post_content
+      response = user.mession.tracked_subscriber_push("open_detail", message, 4, post_id, user[:id], post_channel_id, user.mession[:push_to], user.mession[:push_id])
     elsif base_type == "training"
-      message = user_first_name + " " + user_last_name + " posted a training: " + post_title
-      response = tracked_subscriber_push("open_training", message, 4, post_id, user_id, post_channel_id, user_push_to, user_push_id)
+      message = user[:first_name] + " " + user[:last_name] + " posted a training: " + post_title
+      response = user.mession.tracked_subscriber_push("open_training", message, 4, post_id, user[:id], post_channel_id, user.mession[:push_to], user.mession[:push_id])
     elsif base_type == "schedule"
       if post_content.present?
         message = post_content
       else
-        message = user_first_name + " " + user_last_name + " posted a schedule"
+        message = user[:first_name] + " " + user[:last_name] + " posted a schedule"
       end
-      response = tracked_subscriber_push("open_app", message, 4, post_id, user_id, post_channel_id, user_push_to, user_push_id)
+      response = user.mession.tracked_subscriber_push("open_app", message, 4, post_id, user[:id], post_channel_id, user.mession[:push_to], user.mession[:push_id])
     elsif base_type == "quiz"
-      message = user_first_name + " " + user_last_name + " posted a quiz: " + post_title
-      response = tracked_subscriber_push("open_quiz", message, 4, post_id, user_id, post_channel_id, user_push_to, user_push_id)
+      message = user[:first_name] + " " + user[:last_name] + " posted a quiz: " + post_title
+      response = user.mession.tracked_subscriber_push("open_quiz", message, 4, post_id, user[:id], post_channel_id, user.mession[:push_to], user.mession[:push_id])
     elsif base_type == "shift"
-      message = "#{user_first_name} #{user_last_name} posted a shift trade request. Interested in helping out?"
-      response = tracked_subscriber_push("open_app", message, 4, post_id, user_id, post_channel_id, user_push_to, user_push_id)
+      message = "#{user[:first_name]} #{user[:last_name]} posted a shift trade request. Interested in helping out?"
+      response = user.mession.tracked_subscriber_push("open_app", message, 4, post_id, user[:id], post_channel_id, user.mession[:push_to], user.mession[:push_id])
     else
-      message = user_first_name + " " + user_last_name + " posted: " + post_content
-      response = tracked_subscriber_push("open_app", message, 4, post_id, user_id, post_channel_id, user_push_to, user_push_id)
+      message = user[:first_name] + " " + user[:last_name] + " posted: " + post_content
+      response = user.mession.tracked_subscriber_push("open_app", message, 4, post_id, user[:id], post_channel_id, user.mession[:push_to], user.mession[:push_id])
     end
     if response == 1
       cpr.update_attributes(:attempted => cpr[:attempted] + 1, :success => cpr[:success] + 1)
@@ -70,7 +70,7 @@ class TrackedPushNotificationWorker
       rescue Exception => error
         ErrorLog.create(
           :file => "rb",
-          :function => "tracked_subscriber_push",
+          :function => "user.mession.tracked_subscriber_push",
           :error => "Unable to push to gcm: #{error}")
         if error.includes? "Device token is invalid"
           return -1
@@ -99,7 +99,7 @@ class TrackedPushNotificationWorker
       rescue Exception => error
         ErrorLog.create(
           :file => "rb",
-          :function => "tracked_subscriber_push",
+          :function => "user.mession.tracked_subscriber_push",
           :error => "Unable to push to apns: #{error}")
         if error.includes? "Device token is invalid"
           return -1

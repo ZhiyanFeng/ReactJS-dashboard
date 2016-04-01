5dad2b6c776448a14084dade30b542963ee72123
DANIEL CHEN <daniel85.chen@gmail.com>
2016-04-01 13:09:55 -0700

    detailed report tracker

--- app/workers/slack_channel_push_reporter_worker.rb
+++ app/workers/slack_channel_push_reporter_worker.rb
@@ -16,7 +16,7 @@ class SlackChannelPushReporterWorker
         c.verbose = true
         payload = "{\"attachments\":[{\"fallback\":\"Channel Push Event -- [ID : #{@cpr[:id]}] -- [CHANNEL ID : #{@cpr[:channel_id]}] -- [TARGET SIZE : #{@cpr[:target_number]}] -- [ATTEMPTED : #{@cpr[:attempted]}] -- [SUCCESS : #{@cpr[:success]}] -- [FAILED(MISSING PUSH ID) : #{@cpr[:failed_due_to_missing_id]}] -- [FAILED : #{@cpr[:failed_due_to_other]}] -  \",\"pretext\":\"Channel Push Event Occured #{@cpr[:created_at]}\",\"text\":\"[#{post_title}] - #{post_content}\",\"fields\":[{\"title\":\"ID\",\"value\":\"#{@cpr[:id]}\",\"short\":true},{\"title\":\"Channel ID\",\"value\":\"#{@cpr[:channel_id]}\",\"short\":true},{\"title\":\"Target Size\",\"value\":\"#{@cpr[:target_number]}\",\"short\":true},{\"title\":\"Pushes Attempted\",\"value\":\"#{@cpr[:attempted]}\",\"short\":true},{\"title\":\"Pushes Succeeded\",\"value\":\"#{@cpr[:success]}\",\"short\":true},{\"title\":\"Failed (Missing Push ID)\",\"value\":\"#{@cpr[:failed_due_to_missing_id]}\",\"short\":true},{\"title\":\"Failed (Other)\",\"value\":\"#{@cpr[:failed_due_to_other]}\",\"short\":true}],\"color\":\"#7CD197\"}]}"
         c.http_post(payload)
-        @cpr.update_attribute(@cpr[:is_sent],true)
+        @cpr.update_attribute(:is_sent, true)
       end
     rescue Exception => error
       ErrorLog.create(

6623d9dec2fca6dada20279a9d259eaa05726ba0
DANIEL CHEN <daniel85.chen@gmail.com>
2016-03-31 19:00:09 -0700

    push slack integration

--- app/workers/slack_channel_push_reporter_worker.rb
+++ app/workers/slack_channel_push_reporter_worker.rb
@@ -9,7 +9,7 @@ class SlackChannelPushReporterWorker
       c = Curl::Easy.new
       c.url = "https://hooks.slack.com/services/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"
       c.verbose = true
-      payload = "{\"text\":\"Hello World! #{@cpr[:id]}\",\"icon_emoji\":\":ghost:\"}"
+      payload = "{\"text\":\"Channel Push Event -- [ID : #{@cpr[:id]}] -- [CHANNEL ID : #{@cpr[:channel_id]}] -- [TARGET SIZE : #{@cpr[:target_number]}] -- [ATTEMPTED : #{@cpr[:attempted]}] -- [SUCCESS : #{@cpr[:success]}] -- [FAILED(MISSING PUSH ID) : #{@cpr[:failed_due_to_missing_id]}] -- [FAILED : #{@cpr[:failed_due_to_other]}] -  \",\"icon_emoji\":\":flag-cn:\"}"
       c.http_post(payload)
     rescue Exception => error
       ErrorLog.create(

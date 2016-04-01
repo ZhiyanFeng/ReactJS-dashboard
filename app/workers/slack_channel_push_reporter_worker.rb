33daa3d511f21bdc15bcb4b47130e4c03cb13162
DANIEL CHEN <daniel85.chen@gmail.com>
2016-04-01 12:53:47 -0700

    detailed report tracker

--- app/workers/slack_channel_push_reporter_worker.rb
+++ app/workers/slack_channel_push_reporter_worker.rb
@@ -5,18 +5,18 @@ class SlackChannelPushReporterWorker
   def perform(report_id)
     begin
       @cpr = ChannelPushReport.find(report_id,post_title,post_content)
-      if @cpr[:is_sent]
-        ErrorLog.create(
-          :file => "slack_channel_push_reporter_worker.rb",
-          :function => "perform",
-          :error => "Trying to send a report previously sent with ID: #{@cpr[:id]}")
-      else
+      if !@cpr[:is_sent]
         c = Curl::Easy.new
         c.url = "https://hooks.slack.com/services/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"
         c.verbose = true
         payload = "{\"attachments\":[{\"fallback\":\"Channel Push Event -- [ID : #{@cpr[:id]}] -- [CHANNEL ID : #{@cpr[:channel_id]}] -- [TARGET SIZE : #{@cpr[:target_number]}] -- [ATTEMPTED : #{@cpr[:attempted]}] -- [SUCCESS : #{@cpr[:success]}] -- [FAILED(MISSING PUSH ID) : #{@cpr[:failed_due_to_missing_id]}] -- [FAILED : #{@cpr[:failed_due_to_other]}] -  \",\"pretext\":\"Channel Push Event Occured #{@cpr[:created_at]}\",\"text\":\"[#{post_title}] - #{post_content}\",\"fields\":[{\"title\":\"ID\",\"value\":\"#{@cpr[:id]}\",\"short\":true},{\"title\":\"Channel ID\",\"value\":\"#{@cpr[:channel_id]}\",\"short\":true},{\"title\":\"Target Size\",\"value\":\"#{@cpr[:target_number]}\",\"short\":true},{\"title\":\"Pushes Attempted\",\"value\":\"#{@cpr[:attempted]}\",\"short\":true},{\"title\":\"Pushes Succeeded\",\"value\":\"#{@cpr[:success]}\",\"short\":true},{\"title\":\"Failed (Missing Push ID)\",\"value\":\"#{@cpr[:failed_due_to_missing_id]}\",\"short\":true},{\"title\":\"Failed (Other)\",\"value\":\"#{@cpr[:failed_due_to_other]}\",\"short\":true}],\"color\":\"#7CD197\"}]}"
         c.http_post(payload)
         @cpr.update_attribute(@cpr[:is_sent],true)
+      else
+        ErrorLog.create(
+          :file => "slack_channel_push_reporter_worker.rb",
+          :function => "perform",
+          :error => "Trying to send a report previously sent with ID: #{@cpr[:id]}")
       end
     rescue Exception => error
       ErrorLog.create(

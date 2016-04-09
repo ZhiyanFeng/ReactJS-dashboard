8f7735aa8108ecf1697e0c0648294f3b96b43403
DANIEL CHEN <daniel85.chen@gmail.com>
2016-04-09 14:48:37 -0700

    added title to chat sessions

--- app/serializers/chat_session_serializer.rb
+++ app/serializers/chat_session_serializer.rb
@@ -4,7 +4,12 @@ class ChatSessionSerializer < ActiveModel::Serializer
   :org_id,
   :message_count,
   :participant_count,
+  :chat_title,
   :latest_message,
   :multiuser_chat,
   :updated_at
+
+  def chat_title
+    "Chat~~ PEWPEW"
+  end
 end

c24521e6c2259d34707751ac666bc71fc85fe91f
DANIEL CHEN <daniel85.chen@gmail.com>
2016-08-12 11:29:04 -0700

    changed messaging

--- app/models/user_privilege.rb
+++ app/models/user_privilege.rb
@@ -245,10 +245,7 @@ class UserPrivilege < ActiveRecord::Base
           :location => self[:location_id],
           :owner_id => 134,
           :channel_id => location_channel[:id],
-          #:title => "Welcome to Shyft",
-          #:content => "Begin trading shifts and messaging coworkers today! You now have an exclusive private network for your work location. Click on the 'Contacts' tab, then the '+' button to invite 10 staff members. Have fun!. \nPlease contact hello@myshyft.com for assistance.",
           :title => "Welcome to your Shyft Channel - Earn 25$!",
-          #:content => "This is the main feed for your work location to swap shifts. Use the + button to post a shift, or make a post. You can set up private groups, post schedules, chat, have fun and communicate with your team! Your channel settings are in the top right corner (gear button). Earn 25$ - We want to send your location a gift! Grow this location to 10 team members and Shyft will send a $25 gift card of your choice (Starbucks, Best Buy, Etc.) to your store. All you have to do is invite your team, and submit your store name and address to hello@myshyft.com for our review! *New employee signups only - Ends April 16th at 12:00PM EST* \n\n#ShyftLife 📱🔁📆🙋",
           :content => "This is the main feed for your work location to swap shifts. Use the + button to post a shift, or make a post. You can set up private groups, post schedules, chat, have fun and communicate with your team! Your channel settings are in the top right corner (gear button). Earn 25$ - We want to send your location a gift! Grow this location to 15 team members and Shyft will send a $25 gift card of your choice to your store. All you have to do is invite your team, and submit your store name and address to hello@myshyft.com for our review! ​*New employee signups only - Ends #{(Date.today+5.days).to_formatted_s(:long_ordinal)} at 12:00PM EST* \n\n#ShyftLife 📱🔁📆🙋",
           :post_type => 1
         )

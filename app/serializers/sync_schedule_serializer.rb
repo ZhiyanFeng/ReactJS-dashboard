244cde3d1f73ebe15eb02b203370e218d1abf635
dc85 <daniel85.chen@gmail.com>
2016-08-25 15:29:36 -0700

    added allow delete to schedules

--- app/serializers/sync_schedule_serializer.rb
+++ app/serializers/sync_schedule_serializer.rb
@@ -10,12 +10,23 @@ class SyncScheduleSerializer < ActiveModel::Serializer
   :updated_at,
   :attachment,
   :owner,
+  :allow_delete,
   :is_valid
-  
+
+  def allow_delete
+    if object[:user_id] == object[:owner_id]
+      return true
+    elsif Subscription.exists?(:user_id => object[:user_id], :is_admin => true, :channel_id => object[:channel_id], :is_valid => true)
+      return true
+    else
+      return false
+    end
+  end
+
   def attachment
     if object.attachment_id.presence
       @attachments = Attachment.find(object.attachment_id)
-      @attachments.to_objs      
+      @attachments.to_objs
     end
   end
 
@@ -26,5 +37,5 @@ class SyncScheduleSerializer < ActiveModel::Serializer
   def owner
     OwnerSerializer.new(object.owner)
   end
-  
+
 end

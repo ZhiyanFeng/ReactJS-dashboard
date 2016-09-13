56497ad8a60eb2571584c24d22496e1b5be0d4d5
dc85 <daniel85.chen@gmail.com>
2016-09-13 16:57:49 -0700

    fixed the bug where text announcement is not returned

--- app/controllers/api/ditto/users_controller.rb
+++ app/controllers/api/ditto/users_controller.rb
@@ -304,13 +304,13 @@ module Api
 
           if UserAnalytic.exists?(:action => 1070, :user_id => @user[:id])
             last_fetch = UserAnalytic.where(:action => 1070, :user_id => @user[:id]).last[:created_at]
-            @posts = Post.where("channel_id = #{@subscription[:channel_id]} AND title != 'Shift Trade' AND (z_index < 9999 OR (z_index > 0 AND owner_id = #{@user[:id]})) AND post_type in (#{@@_BASIC_POST_TYPE_IDS + @@_ANNOUNCEMENT_POST_TYPE_IDS}) AND is_valid").order("created_at DESC").limit(10)
+            @posts = Post.where("channel_id = #{@subscription[:channel_id]} AND title != 'Shift Trade' AND (z_index < 9999 OR (z_index > 0 AND owner_id = #{@user[:id]})) AND post_type in (#{@@_BASIC_POST_TYPE_IDS},#{@@_ANNOUNCEMENT_POST_TYPE_IDS}) AND is_valid").order("created_at DESC").limit(10)
           else
             last_fetch = Time.now.utc
-            @posts = Post.where("channel_id = #{@subscription[:channel_id]} AND title != 'Shift Trade' AND (z_index < 9999 OR (z_index > 0 AND owner_id = #{@user[:id]})) AND post_type in (#{@@_BASIC_POST_TYPE_IDS + @@_ANNOUNCEMENT_POST_TYPE_IDS}) AND is_valid").order("created_at DESC").limit(10)
+            @posts = Post.where("channel_id = #{@subscription[:channel_id]} AND title != 'Shift Trade' AND (z_index < 9999 OR (z_index > 0 AND owner_id = #{@user[:id]})) AND post_type in (#{@@_BASIC_POST_TYPE_IDS},#{@@_ANNOUNCEMENT_POST_TYPE_IDS}) AND is_valid").order("created_at DESC").limit(10)
           end
 
-          deleted_ids = Post.where("channel_id = #{@subscription[:channel_id]} AND title != 'Shift Trade' AND (z_index < 9999 OR (z_index > 0 AND owner_id = #{@user[:id]})) AND post_type in (#{@@_BASIC_POST_TYPE_IDS + @@_ANNOUNCEMENT_POST_TYPE_IDS}) AND is_valid = 'f' AND updated_at > '#{last_fetch}'").pluck(:id)
+          deleted_ids = Post.where("channel_id = #{@subscription[:channel_id]} AND title != 'Shift Trade' AND (z_index < 9999 OR (z_index > 0 AND owner_id = #{@user[:id]})) AND post_type in (#{@@_BASIC_POST_TYPE_IDS},#{@@_ANNOUNCEMENT_POST_TYPE_IDS}) AND is_valid = 'f' AND updated_at > '#{last_fetch}'").pluck(:id)
 
           UserAnalytic.create(:action => 1070, :org_id => 1, :user_id => @user[:id], :ip_address => request.remote_ip.to_s)
 
@@ -343,7 +343,7 @@ module Api
 
           @subscription = Subscription.where(:id => params[:subscription_id], :is_valid => true).first
 
-          @posts = Post.where("channel_id = #{@subscription[:channel_id]} AND title != 'Shift Trade' AND z_index < 9999 AND post_type in (#{@@_BASIC_POST_TYPE_IDS + @@_ANNOUNCEMENT_POST_TYPE_IDS}) AND is_valid AND id < #{params[:last_post_id]}").order("created_at DESC").limit(10)
+          @posts = Post.where("channel_id = #{@subscription[:channel_id]} AND title != 'Shift Trade' AND (z_index < 9999 OR (z_index > 0 AND owner_id = #{@user[:id]})) AND post_type in (#{@@_BASIC_POST_TYPE_IDS},#{@@_ANNOUNCEMENT_POST_TYPE_IDS}) AND is_valid AND id < #{params[:last_post_id]}").order("created_at DESC").limit(10)
 
           @posts.each do |post|
             post.check_user(params[:id])
@@ -490,7 +490,7 @@ module Api
           if @channel[:channel_type] == "public_feed" || @channel[:channel_type] == "organization_feed"
             if Subscription.exists?(:channel_id => params[:channel_id], :user_id => @user[:id])
               @subscription = Subscription.where(:channel_id => params[:channel_id], :user_id => @user[:id]).first
-              @subscription.update_attributes(:is_valid => true, :is_valid => true)
+              @subscription.update_attributes(:is_valid => true, :is_active => true)
               render json: { "eXpresso" => { "code" => 1, "message" => "Successfully subscribed to channel!" } }
             else
               @subscription = Subscription.create(

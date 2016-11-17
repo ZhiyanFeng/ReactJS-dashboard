4b5c15717a1ba45afc746a980b8350c557e15a9c
dc85 <daniel85.chen@gmail.com>
2016-11-17 04:18:16 -0500

    changed title and removed at

--- app/models/user_privilege.rb
+++ app/models/user_privilege.rb
@@ -247,9 +247,9 @@ class UserPrivilege < ActiveRecord::Base
           :location => self[:location_id],
           :owner_id => 134,
           :channel_id => location_channel[:id],
-          :title => "Welcome to Shyft - Earn 25$!",
+          :title => "Welcome to Shyft!",
           #:content => "This is the main feed for your work location to swap shifts. Use the + button to post a shift, or make a post. You can set up private groups, post schedules, chat, have fun and communicate with your team! Your channel settings are in the top right corner (gear button). Earn 25$ - We want to send your location a gift! Grow this location to 15 team members and Shyft will send a $25 gift card of your choice to your store. All you have to do is invite your team, and submit your store name and address to hello@myshyft.com for our review! ​*New employee signups only - Ends #{(Date.today+5.days).to_formatted_s(:long_ordinal)} at 12:00PM EST* \n\n#ShyftLife 📱🔁📆🙋",
-          :content => "Welcome to Shyft! We hope we make work a little easier for you. To have a digital $20 Gift card emailed to you, simply do these two things before #{(Date.today+5.days).to_formatted_s(:long_ordinal)} at 12:00PM EST* \n\n1. Get 10 coworkers to join this location.\n2. Have you leader claim admin status in your channel settings.\nThen email us your store name to hello@myshyft.com\n*Limit of one gift card per location*\nThe Shyft Team 📱📆🔄😎",
+          :content => "Welcome to Shyft! We hope we make work a little easier for you. To have a digital $20 Gift card emailed to you, simply do these two things before #{(Date.today+5.days).to_formatted_s(:long_ordinal)} 12:00PM EST* \n\n1. Get 10 coworkers to join this location.\n2. Have you leader claim admin status in your channel settings.\nThen email us your store name to hello@myshyft.com\n*Limit of one gift card per location*\nThe Shyft Team 📱📆🔄😎",
           :post_type => 1
         )
         @post.save

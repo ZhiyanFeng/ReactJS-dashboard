2a76c1d09f1bebd0b4ba4cbd74dbb12fe6bfd85c
dc85 <daniel85.chen@gmail.com>
2016-12-14 11:59:59 -0800

    LocationSwiftSearchSerializer fix

--- app/serializers/location_swift_search_serializer.rb
+++ app/serializers/location_swift_search_serializer.rb
@@ -8,16 +8,16 @@ class LocationSwiftSearchSerializer < ActiveModel::Serializer
   :admin_profile
 
   def admin_name
-    if UserPrivilege.exists?(:location_id => self[:id], :is_admin => true, :is_valid => true)
-      priv = UserPrivilege.where(["location_id = ? AND is_admin AND is_valid", self[:id]]).first
+    if UserPrivilege.exists?(:location_id => object[:id], :is_admin => true, :is_valid => true)
+      priv = UserPrivilege.where(["location_id = ? AND is_admin AND is_valid", object[:id]]).first
       user = User.find(priv[:owner_id])
       user[:first_name]
     end
   end
 
   def admin_profile
-    if UserPrivilege.exists?(:location_id => self[:id], :is_admin => true, :is_valid => true)
-      priv = UserPrivilege.where(["location_id = ? AND is_admin AND is_valid", self[:id]]).first
+    if UserPrivilege.exists?(:location_id => object[:id], :is_admin => true, :is_valid => true)
+      priv = UserPrivilege.where(["location_id = ? AND is_admin AND is_valid", object[:id]]).first
       user = User.find(priv[:owner_id])
       user.profile_image.thumb_url
     end
